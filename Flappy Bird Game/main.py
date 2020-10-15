7e46b702f9dee221f101b0a73cf587f6022e577e
Devam Vyas <71130376+Devam-Vyas@users.noreply.github.com>
2020-10-15 14:56:45 +0530

    Update main.py

--- Flappy Bird Game/main.py
+++ Flappy Bird Game/main.py
@@ -1,5 +1,4 @@
 #agar ap convert alpha pixels ke alpha bhi change honge aur agar sirf convert ka istemal karoge to image ke pixals change for blitting 
-#agar ap convert alpha pixels ke alpha bhi change honge aur agar sirf convert ka istemal karoge to image ke pixals change for blitting 
 import random # For generating random numbers
 import sys # We will use sys.exit to exit the program
 import pygame
